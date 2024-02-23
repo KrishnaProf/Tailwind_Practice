d1c766b7cbe38ae2271494ecc525a86bb8b596e3
Krishna <leelakrishna682@gmail.com>
2024-02-23 12:54:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <>
       <div className='flex justify-between grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-        <div className='bg-red-500'>Hii</div>
-        <div className='bg-yellow-500 '>Hi</div>
-        <div className='bg-blue-500'>Hi</div>
-        <div className='bg-blue-500'>Hi</div>
+        <div className='bg-red-500'>Hii There</div>
+        <div className='bg-yellow-500 '>Hi There</div>
+        <div className='bg-blue-500'>Hi There</div>
+        <div className='bg-blue-500'>Hi There</div>
 
 
 
