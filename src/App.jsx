75604cf3eb7df1bb2184e973ba33b05c27cf882c
Leela_Krishna <leela_krishna@dell.com>
2024-02-23 12:53:18 +0530

    Tailwind CSS updated

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex justify-between grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+        <div className='bg-red-500'>Hii</div>
+        <div className='bg-yellow-500 '>Hi</div>
+        <div className='bg-blue-500'>Hi</div>
+        <div className='bg-blue-500'>Hi</div>
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/RevenueCard.jsx
@@ -0,0 +1,5 @@
+export const RevenueCard = () => {
+  return (
+    <div className='bg-red-500'>Hii</div>
+  )
+}
